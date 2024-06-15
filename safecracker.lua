627d85a817fb5b24b3d60ab7acba49cc6f14a9c8
Kanarz <107653172+Kanarz@users.noreply.github.com>
2024-06-15 13:40:46 -0400

    Create safecracker.lua
    safecracker, should work, you just gotta be light speed

---
+++ safecracker.lua
@@ -0,0 +1,287 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local _10 = Instance.new("TextButton")
+local _20 = Instance.new("TextButton")
+local _30 = Instance.new("TextButton")
+local _40 = Instance.new("TextButton")
+local _50 = Instance.new("TextButton")
+local _60 = Instance.new("TextButton")
+local _70 = Instance.new("TextButton")
+local _80 = Instance.new("TextButton")
+local _90 = Instance.new("TextButton")
+local _0 = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(94, 94, 94)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.0303030312, 0, 0.150375932, 0)
+Frame.Size = UDim2.new(0, 385, 0, 238)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(-0.000261737761, 0, -0.000884560985, 0)
+TextLabel.Size = UDim2.new(0, 122, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "safe cracker 69420"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+_10.Name = "10"
+_10.Parent = Frame
+_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_10.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_10.BorderSizePixel = 0
+_10.Position = UDim2.new(0.89751333, 0, 0.898275197, 0)
+_10.Size = UDim2.new(0, 39, 0, 24)
+_10.Font = Enum.Font.SourceSans
+_10.Text = "10"
+_10.TextColor3 = Color3.fromRGB(0, 0, 0)
+_10.TextSize = 14.000
+
+_20.Name = "20"
+_20.Parent = Frame
+_20.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_20.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_20.BorderSizePixel = 0
+_20.Position = UDim2.new(0.89751333, 0, 0.797434807, 0)
+_20.Size = UDim2.new(0, 39, 0, 24)
+_20.Font = Enum.Font.SourceSans
+_20.Text = "20"
+_20.TextColor3 = Color3.fromRGB(0, 0, 0)
+_20.TextSize = 14.000
+
+_30.Name = "30"
+_30.Parent = Frame
+_30.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_30.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_30.BorderSizePixel = 0
+_30.Position = UDim2.new(0.89751333, 0, 0.709199548, 0)
+_30.Size = UDim2.new(0, 39, 0, 24)
+_30.Font = Enum.Font.SourceSans
+_30.Text = "30"
+_30.TextColor3 = Color3.fromRGB(0, 0, 0)
+_30.TextSize = 14.000
+
+_40.Name = "40"
+_40.Parent = Frame
+_40.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_40.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_40.BorderSizePixel = 0
+_40.Position = UDim2.new(0.89751333, 0, 0.608359218, 0)
+_40.Size = UDim2.new(0, 39, 0, 24)
+_40.Font = Enum.Font.SourceSans
+_40.Text = "40"
+_40.TextColor3 = Color3.fromRGB(0, 0, 0)
+_40.TextSize = 14.000
+
+_50.Name = "50"
+_50.Parent = Frame
+_50.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_50.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_50.BorderSizePixel = 0
+_50.Position = UDim2.new(0.89751333, 0, 0.507518888, 0)
+_50.Size = UDim2.new(0, 39, 0, 24)
+_50.Font = Enum.Font.SourceSans
+_50.Text = "50"
+_50.TextColor3 = Color3.fromRGB(0, 0, 0)
+_50.TextSize = 14.000
+
+_60.Name = "60"
+_60.Parent = Frame
+_60.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_60.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_60.BorderSizePixel = 0
+_60.Position = UDim2.new(0.89751333, 0, 0.406678528, 0)
+_60.Size = UDim2.new(0, 39, 0, 24)
+_60.Font = Enum.Font.SourceSans
+_60.Text = "60"
+_60.TextColor3 = Color3.fromRGB(0, 0, 0)
+_60.TextSize = 14.000
+
+_70.Name = "70"
+_70.Parent = Frame
+_70.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_70.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_70.BorderSizePixel = 0
+_70.Position = UDim2.new(0.89751333, 0, 0.305838197, 0)
+_70.Size = UDim2.new(0, 39, 0, 24)
+_70.Font = Enum.Font.SourceSans
+_70.Text = "70"
+_70.TextColor3 = Color3.fromRGB(0, 0, 0)
+_70.TextSize = 14.000
+
+_80.Name = "80"
+_80.Parent = Frame
+_80.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_80.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_80.BorderSizePixel = 0
+_80.Position = UDim2.new(0.89751333, 0, 0.204997852, 0)
+_80.Size = UDim2.new(0, 39, 0, 24)
+_80.Font = Enum.Font.SourceSans
+_80.Text = "80"
+_80.TextColor3 = Color3.fromRGB(0, 0, 0)
+_80.TextSize = 14.000
+
+_90.Name = "90"
+_90.Parent = Frame
+_90.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_90.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_90.BorderSizePixel = 0
+_90.Position = UDim2.new(0.89751333, 0, 0.104157522, 0)
+_90.Size = UDim2.new(0, 39, 0, 24)
+_90.Font = Enum.Font.SourceSans
+_90.Text = "90"
+_90.TextColor3 = Color3.fromRGB(0, 0, 0)
+_90.TextSize = 14.000
+
+_0.Name = "0"
+_0.Parent = Frame
+_0.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+_0.BorderColor3 = Color3.fromRGB(0, 0, 0)
+_0.BorderSizePixel = 0
+_0.Position = UDim2.new(0.89751333, 0, 0.00331718381, 0)
+_0.Size = UDim2.new(0, 39, 0, 24)
+_0.Font = Enum.Font.SourceSans
+_0.Text = "0"
+_0.TextColor3 = Color3.fromRGB(0, 0, 0)
+_0.TextSize = 14.000
+
+-- Scripts:
+
+local function RTFFLV_fake_script() -- _10.LocalScript 
+	local script = Instance.new('LocalScript', _10)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = -38
+	end)
+end
+coroutine.wrap(RTFFLV_fake_script)()
+local function GTUJ_fake_script() -- _20.LocalScript 
+	local script = Instance.new('LocalScript', _20)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = -70
+	end)
+end
+coroutine.wrap(GTUJ_fake_script)()
+local function KCPTP_fake_script() -- _30.LocalScript 
+	local script = Instance.new('LocalScript', _30)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = -105
+	end)
+end
+coroutine.wrap(KCPTP_fake_script)()
+local function LQLFPUE_fake_script() -- _40.LocalScript 
+	local script = Instance.new('LocalScript', _40)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = -145
+	end)
+end
+coroutine.wrap(LQLFPUE_fake_script)()
+local function BBFIH_fake_script() -- _50.LocalScript 
+	local script = Instance.new('LocalScript', _50)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = -180
+	end)
+end
+coroutine.wrap(BBFIH_fake_script)()
+local function IWNCNSZ_fake_script() -- _60.LocalScript 
+	local script = Instance.new('LocalScript', _60)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = 145
+	end)
+end
+coroutine.wrap(IWNCNSZ_fake_script)()
+local function QQDITL_fake_script() -- _70.LocalScript 
+	local script = Instance.new('LocalScript', _70)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = 105
+	end)
+end
+coroutine.wrap(QQDITL_fake_script)()
+local function ONSBVG_fake_script() -- _80.LocalScript 
+	local script = Instance.new('LocalScript', _80)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = 70
+	end)
+end
+coroutine.wrap(ONSBVG_fake_script)()
+local function EONIB_fake_script() -- _90.LocalScript 
+	local script = Instance.new('LocalScript', _90)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = 38
+	end)
+end
+coroutine.wrap(EONIB_fake_script)()
+local function UDKWT_fake_script() -- _0.LocalScript 
+	local script = Instance.new('LocalScript', _0)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		game.Players.LocalPlayer.PlayerGui.GameMenus.Safe.Safe.Dial.Rotation = 0
+	end)
+end
+coroutine.wrap(UDKWT_fake_script)()
+local function UEFMG_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+			
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(UEFMG_fake_script)()
